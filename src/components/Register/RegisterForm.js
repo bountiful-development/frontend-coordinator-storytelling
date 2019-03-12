54719c44ff15fa18390df15716cb08b45fba96f6
Ryan Walker <rytwalker@gmail.com>
2019-03-12 13:02:24 -0400

    register page done

--- src/components/Register/RegisterForm.js
+++ src/components/Register/RegisterForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import logo from '../../img/logo.svg';
 // import FormGroupInput from '../Form/FormGroupInput';
 
 const countries = [
@@ -39,10 +41,13 @@ class RegisterForm extends Component {
   render() {
     const { name, country, title, email, password } = this.state;
     return (
-      <form>
-        <h1>
-          Sign Up <span>As A Coordinator</span>
-        </h1>
+      <StyledRegisterForm>
+        <RegisterHeading>
+          <img src={logo} alt="logo" />
+          <span className="text">
+            Sign Up <span>As A Coordinator</span>
+          </span>
+        </RegisterHeading>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
           <input
@@ -102,14 +107,103 @@ class RegisterForm extends Component {
             required
           />
         </div>
-        <button type="submit">Sign Up</button>
-        <div>
+        <SubmitButton type="submit">Sign Up</SubmitButton>
+        <Redirect>
           <span>Already signed up?</span>
           <Link to="/login">Login</Link>
-        </div>
-      </form>
+        </Redirect>
+      </StyledRegisterForm>
     );
   }
 }
 
+const StyledRegisterForm = styled.form`
+  background: #fff;
+  padding: 2rem 5rem;
+  width: 600px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
+
+  .form-group {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2rem;
+    label {
+      text-transform: uppercase;
+      font-weight: 700;
+    }
+    input,
+    select {
+      background: #fafafa;
+      border-radius: 5px;
+      padding: 1rem;
+      font-size: inherit;
+      width: 100%;
+      border: 1px solid #e6e6e6;
+      font-family: inherit;
+      &:focus {
+        outline: none;
+        border: 1px solid #ed9728;
+      }
+    }
+
+    select {
+      height: 3.6rem;
+    }
+  }
+`;
+
+const SubmitButton = styled.button`
+  background: #ed9728;
+  font-size: inherit;
+  padding: 1rem 1.5rem;
+  display: flex;
+  text-transform: uppercase;
+  font-family: inherit;
+  color: #fff;
+  font-weight: 700;
+  border: transparent;
+  border-radius: 5px;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.1);
+  margin: auto;
+  margin-bottom: 3rem;
+  cursor: pointer;
+  transition: all 0.2s;
+  &:hover {
+    background: #edaf5f;
+  }
+`;
+
+const RegisterHeading = styled.h1`
+  text-transform: uppercase;
+  flex-wrap: wrap;
+  margin-bottom: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 68px;
+    height: 68px;
+    /* margin-right: 2px; */
+  }
+  .text {
+    font-size: 3.6rem;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    span {
+      font-size: 1.5rem;
+      font-weight: 400;
+      /* width: 100%; */
+    }
+  }
+`;
+
+const Redirect = styled.div`
+  font-size: 1.4rem;
+  text-align: center;
+  a {
+    margin-left: 8px;
+    color: #ed9728;
+  }
+`;
 export default RegisterForm;
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import LoginForm from '../components/Login/LoginForm';
 
 class LoginView extends Component {
   render() {
     return (
-      <div>
+      <StyledLoginView>
         <h1>Donor Homepage</h1>
         <LoginForm />
-      </div>
+      </StyledLoginView>
     );
   }
 }
+
+const StyledLoginView = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
 export default LoginView;

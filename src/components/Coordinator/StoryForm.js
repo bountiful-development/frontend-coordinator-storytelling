533f9d27c5f2e54fe90455f8f546de86d760cc5c
Ryan Walker <rytwalker@gmail.com>
2019-03-14 09:28:19 -0400

    state set up in add new story form

--- src/components/Coordinator/StoryForm.js
+++ src/components/Coordinator/StoryForm.js
@@ -4,16 +4,30 @@ import { Form } from '../Form/Form';
 import { ViewButton } from '../Card/Card';
 
 class StoryForm extends Component {
+  state = { title: '', story: '' };
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
   render() {
+    const { title, story } = this.state;
     return (
       <AddStoryForm>
         <div className="form-group">
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={this.handleChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="story">Story:</label>
-          <textarea name="story" id="story" />
+          <textarea
+            name="story"
+            id="story"
+            value={story}
+            onChange={this.handleChange}
+          />
         </div>
         <ViewButton type="submit">Add Story</ViewButton>
       </AddStoryForm>

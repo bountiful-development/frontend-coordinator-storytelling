bdf8ab4e23f7d652085bf8531f4372117231fd87
Ryan Walker <rytwalker@gmail.com>
2019-03-14 10:34:22 -0400

    story saved

--- src/components/Donor/Story/StorySidebar.js
+++ src/components/Donor/Story/StorySidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Card } from '../../../components/Card/Card';
+import { Card } from '../../../DonorCardscomponents/Card/Card';
 
 const StorySidebar = () => {
   return (
